495a55ee8cc72bd0efef9130941dd180840f269c
daichaoxing <daichaoxing@gmail.com>
2015-01-23 04:36:57 -0600

    Added squarem2,fpiter with examples.

--- src/cxx_SQUAREM.cpp
+++ src/cxx_SQUAREM.cpp
@@ -1,16 +1,13 @@
 //TODO:
-//1. example of fixptfn and objfn need to be added 1. Done
-//2. create class object
-//3. cyclem equivalent
-//4. combine squarem1&2 by defining the default objfn
-//5. matrix/vector operation without loop
+//1. create class object
+//2. cyclem equivalent
+//3. matrix/vector operation without loop
 #include <iostream>
 #include <algorithm>
 #include <cmath>
 #include <math.h>
 #include <vector>
 #include <numeric>
-
 
 using namespace std;
 
@@ -94,7 +91,7 @@ SquaremOutput squarem1(std::vector<double> par){
         sr2_scalar=0;
         for (int i=0;i<parvectorlength;i++){sr2_scalar+=pow(p1cpp[i]-pcpp[i],2);}
         if(sqrt(sr2_scalar)<SquaremDefault.tol){break;}
-
+        
         //Step 2
         try{p2cpp=fixptfn(p1cpp);feval++;}
         catch(...){
@@ -110,7 +107,7 @@ SquaremOutput squarem1(std::vector<double> par){
         srv_scalar=0;
         for (int i=0;i<parvectorlength;i++){srv_scalar+=(p2cpp[i]-2*p1cpp[i]+pcpp[i])*(p1cpp[i]-pcpp[i]);}
         //std::cout<<"sr2,sv2,srv="<<sr2_scalar<<","<<sv2_scalar<<","<<srv_scalar<<std::endl;//debugging
-
+        
         //Step 3 Proposing new value
         switch (SquaremDefault.method){
             case 1: alpha= -srv_scalar/sv2_scalar;
@@ -230,6 +227,155 @@ SquaremOutput squarem1(std::vector<double> par){
 }
 
 
+SquaremOutput squarem2(std::vector<double> par){
+    double res,parnorm,kres;
+    std::vector<double> pcpp,p1cpp,p2cpp,pnew,ptmp;
+    std::vector<double> q1,q2,sr2,sq2,sv2,srv;
+    double sr2_scalar,sq2_scalar,sv2_scalar,srv_scalar,alpha,stepmin,stepmax;
+    int iter,feval;
+    bool conv,extrap;
+    stepmin=SquaremDefault.stepmin0;
+    stepmax=SquaremDefault.stepmax0;
+    if(SquaremDefault.trace){std::cout<<"Squarem-1"<<std::endl;}
+    
+    iter=1;pcpp=par;pnew=par;
+    feval=0;conv=true;
+    
+    const long int parvectorlength=pcpp.size();
+    
+    while(feval<SquaremDefault.maxiter){
+        //Step 1
+        extrap = true;
+        try{p1cpp=fixptfn(pcpp);feval++;}
+        catch(...){
+            std::cout<<"Error in fixptfn function evaluation";
+            return sqobjnull;
+        }
+        
+        sr2_scalar=0;
+        for (int i=0;i<parvectorlength;i++){sr2_scalar+=pow(p1cpp[i]-pcpp[i],2);}
+        if(sqrt(sr2_scalar)<SquaremDefault.tol){break;}
+        
+        //Step 2
+        try{p2cpp=fixptfn(p1cpp);feval++;}
+        catch(...){
+            std::cout<<"Error in fixptfn function evaluation";
+            return sqobjnull;
+        }
+        sq2_scalar=0;
+        for (int i=0;i<parvectorlength;i++){sq2_scalar+=pow(p2cpp[i]-p1cpp[i],2);}
+        sq2_scalar=sqrt(sq2_scalar);
+        if (sq2_scalar<SquaremDefault.tol){break;}
+        res=sq2_scalar;
+        
+        sv2_scalar=0;
+        for (int i=0;i<parvectorlength;i++){sv2_scalar+=pow(p2cpp[i]-2*p1cpp[i]+pcpp[i],2);}
+        srv_scalar=0;
+        for (int i=0;i<parvectorlength;i++){srv_scalar+=(p2cpp[i]-2*p1cpp[i]+pcpp[i])*(p1cpp[i]-pcpp[i]);}
+        //std::cout<<"sr2,sv2,srv="<<sr2_scalar<<","<<sv2_scalar<<","<<srv_scalar<<std::endl;//debugging
+        
+        //Step 3 Proposing new value
+        switch (SquaremDefault.method){
+            case 1: alpha= -srv_scalar/sv2_scalar;
+            case 2: alpha= -sr2_scalar/srv_scalar;
+            case 3: alpha= sqrt(sr2_scalar/sv2_scalar);
+        }
+        
+        alpha=std::max(stepmin,std::min(stepmax,alpha));
+        //std::cout<<"alpha="<<alpha<<std::endl;//debugging
+        for (int i=0;i<parvectorlength;i++){pnew[i]=pcpp[i]+2.0*alpha*(p1cpp[i]-pcpp[i])+pow(alpha,2)*(p2cpp[i]-2*p1cpp[i]+pcpp[i]);}
+        //pnew = pcpp + 2.0*alpha*q1 + alpha*alpha*(q2-q1);
+        
+        //Step 4 stabilization
+        if(std::abs(alpha-1)>0.01){
+            try{ptmp=fixptfn(pnew);feval++;}
+            catch(...){
+                pnew=p2cpp;
+                if(alpha==stepmax){
+                    stepmax=std::max(SquaremDefault.stepmax0,stepmax/SquaremDefault.mstep);
+                }
+                alpha=1;
+                extrap=false;
+                if(alpha==stepmax){stepmax=SquaremDefault.mstep*stepmax;}
+                if(stepmin<0 && alpha==stepmin){stepmin=SquaremDefault.mstep*stepmin;}
+                pcpp=pnew;
+                if(SquaremDefault.trace){std::cout<<"Residual: "<<res<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<std::endl;}
+                iter++;
+                continue;//next round in while loop
+            }
+            res=0;
+            for (int i=0;i<parvectorlength;i++){res+=pow(ptmp[i]-pnew[i],2);}
+            res=sqrt(res);
+            parnorm=0;
+            for (int i=0;i<parvectorlength;i++){parnorm+=pow(p2cpp[i],2);}
+            parnorm=sqrt(parnorm/parvectorlength);
+            kres=SquaremDefault.kr*(1+parnorm)+sq2_scalar;
+            if(res <= kres){
+                pnew=ptmp;
+            }else{
+                pnew=p2cpp;
+                if(alpha==stepmax){stepmax=SquaremDefault.mstep*stepmax;}
+                alpha=1;
+                extrap=false;
+            }
+        }
+        
+        if(alpha==stepmax){stepmax=SquaremDefault.mstep*stepmax;}
+        if(stepmin<0 && alpha==stepmin){stepmin=SquaremDefault.mstep*stepmin;}
+        
+        pcpp=pnew;
+        if(SquaremDefault.trace){std::cout<<"Residual: "<<res<<"  Extrapolation: "<<extrap<<"  Steplength: "<<alpha<<std::endl;}
+        iter++;
+    }
+    
+    if (feval >= SquaremDefault.maxiter){conv=false;}
+    
+    //assigning values
+    sqobj.par=pcpp;
+    sqobj.valueobjfn=NAN;
+    sqobj.iter=iter;
+    sqobj.pfevals=feval;
+    sqobj.objfevals=0;
+    sqobj.convergence=conv;
+    return(sqobj);
+}
+
+SquaremOutput fpiter(std::vector<double> par){
+    //std::vector<double> p,p1,p2;//R data types
+    double lnewcpp,res;
+    std::vector<double> pcpp,pnew;
+    int iter;
+    bool conv;
+    if(SquaremDefault.trace){std::cout<<"Standaard Fixed Point Iteration:"<<std::endl;}
+    iter=1;res=NAN;conv=false;
+    pcpp=par;
+    const long int parvectorlength=pcpp.size();
+    while(iter<SquaremDefault.maxiter*4){
+        pnew=fixptfn(pcpp);
+        res=0;
+        for (int i=0;i<parvectorlength;i++){res+=pow(pnew[i]-pcpp[i],2);}
+        res=sqrt(res);
+        if(res<SquaremDefault.tol){conv=true;break;}
+        if(SquaremDefault.trace){
+            if(fmod(iter,100)==0){
+                std::cout<<"Iter: "<<iter<<"Residual: "<<res<<std::endl;
+            }
+        }
+        pcpp=pnew;
+        iter++;
+    }
+    
+    lnewcpp=objfn(pcpp);
+    //assigning values
+    sqobj.par=pcpp;
+    sqobj.valueobjfn=lnewcpp;
+    sqobj.iter=iter;
+    sqobj.pfevals=iter;
+    sqobj.objfevals=1;
+    sqobj.convergence=conv;
+    return(sqobj);
+}
+
 
 //main() used for demostration
 
@@ -238,7 +384,10 @@ std::vector<int> Other_input1 {162,267,271,185,111,61,27,8,3,1};
 int main(){
     std::cout<<"Hi, this is a demostration using Poisson mixture!"<<std::endl;
     std::vector<double> par_initial {0.5,3,1};//some random starting point,
-    SquaremOutput SQ_result=cxxSQUAREM(par_initial);
+    SquaremOutput SQ_result;
+    
+    std::cout<<"\n\n\nDemo1: using squarem1 and the objective function"<<endl;
+    SQ_result=squarem1(par_initial);
     cout<<"Initial input vector:"<<endl;
     for(int i=0;i<par_initial.size();i++) cout<<par_initial[i]<<" ";
     cout<<"\nOutput vector by SQUAREM:"<<endl;
@@ -247,6 +396,29 @@ int main(){
     cout<<SQ_result.pfevals<<endl;
     cout<<"Number of evaluations on objective function:"<<endl;
     cout<<SQ_result.objfevals<<endl;
+    
+    std::cout<<"\n\n\nDemo2: using squarem2 without objective function"<<endl;
+    SQ_result=squarem2(par_initial);
+    cout<<"Initial input vector:"<<endl;
+    for(int i=0;i<par_initial.size();i++) cout<<par_initial[i]<<" ";
+    cout<<"\nOutput vector by SQUAREM:"<<endl;
+    for(int i=0;i<par_initial.size();i++) cout<<SQ_result.par[i]<<" ";
+    cout<<"\nNumber of evaluations on fixed point function:"<<endl;
+    cout<<SQ_result.pfevals<<endl;
+    cout<<"Number of evaluations on objective function:"<<endl;
+    cout<<SQ_result.objfevals<<endl;
+    
+    std::cout<<"\n\n\nDemo3: using simple fixed point iteration"<<endl;
+    SQ_result=fpiter(par_initial);
+    cout<<"Initial input vector:"<<endl;
+    for(int i=0;i<par_initial.size();i++) cout<<par_initial[i]<<" ";
+    cout<<"\nOutput vector by SQUAREM:"<<endl;
+    for(int i=0;i<par_initial.size();i++) cout<<SQ_result.par[i]<<" ";
+    cout<<"\nNumber of evaluations on fixed point function:"<<endl;
+    cout<<SQ_result.pfevals<<endl;
+    cout<<"Number of evaluations on objective function:"<<endl;
+    cout<<SQ_result.objfevals<<endl;
+    
     return 0;
 }
 
@@ -294,8 +466,8 @@ double objfn(std::vector<double> par){
     double objvalue=0;
     vector<double> loglik(Other_input1.size());
     for (int i=0;i<Other_input1.size();i++){
-    loglik[i]=Other_input1[i]*log(par[0]*exp(-par[1])*pow(par[1],i)/exp(lgamma(i+1))+
-        (1-par[0])*exp(-par[2])*pow(par[2],i)/exp(lgamma(i+1)));
+        loglik[i]=Other_input1[i]*log(par[0]*exp(-par[1])*pow(par[1],i)/exp(lgamma(i+1))+
+                                      (1-par[0])*exp(-par[2])*pow(par[2],i)/exp(lgamma(i+1)));
         objvalue-=loglik[i];
     }
     return objvalue;
